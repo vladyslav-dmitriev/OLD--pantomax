e3a0745a3a69020430c2cf4711d791a7534dfd12
Vladyslav <vladyslav.dmitriev@gmail.com>
2018-04-06 13:49:45 +0300

    correction

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import 'normalize.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap";
-import 'owl.carousel/dist/assets/owl.carousel.min.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
-import 'owl.carousel';
 import './sass/main.sass';
