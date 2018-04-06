ec5c7ecba335c1474c30a45001d6f4b6f21958d3
Vladyslav <vladyslav.dmitriev@gmail.com>
2018-04-06 13:33:04 +0300

    add an adaptive design

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import 'normalize.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap";
+import 'owl.carousel/dist/assets/owl.carousel.min.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import 'owl.carousel';
 import './sass/main.sass';
-
