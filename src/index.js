30996088e9b17b0fe93ece2adc02b53a9b5093fe
Vladyslav <vladyslav.dmitriev@gmail.com>
2018-04-06 08:21:55 +0300

    finished version for PC

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import 'normalize.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap";
-import 'font-awesome/scss/font-awesome.scss'
 import './sass/main.sass';
 
-console.log('in index.js');
-console.log($);
-console.log(jQuery);
